4db2e257cec5ef38fea5c890f381fef3dac2249c
Phyo Wai Yan Lin Zaw <phyowaiyanlinzaw@ucsy.edu.mm>
2023-10-10 10:49:58 +0630

    Created User and Department Entities

--- src/main/java/team/placeholder/internalprojectsmanagementsystem/model/department/Department.java
+++ src/main/java/team/placeholder/internalprojectsmanagementsystem/model/department/Department.java
@@ -1,8 +1,18 @@
 package team.placeholder.internalprojectsmanagementsystem.model.department;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Entity
+@Data
+@NoArgsConstructor
 public class Department {
+
+    @Id
+    private Long id;
+
 
     //TODO: Implement Department Entity
 }
--- src/main/java/team/placeholder/internalprojectsmanagementsystem/model/user/User.java
+++ src/main/java/team/placeholder/internalprojectsmanagementsystem/model/user/User.java
@@ -1,6 +1,31 @@
 package team.placeholder.internalprojectsmanagementsystem.model.user;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import team.placeholder.internalprojectsmanagementsystem.model.department.Department;
+import team.placeholder.internalprojectsmanagementsystem.model.user.userenums.Role;
+
+@Entity
+@Data
+@NoArgsConstructor
 public class User {
 
-    //TODO: Implement User Entity
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Column(unique=true)
+    private String email;
+
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @ManyToOne
+    private Department department;
+
 }
