921d56b798047b1ca00528b7029943ff322cea68
Phyo Wai Yan Lin Zaw <phyowaiyanlinzaw@ucsy.edu.mm>
2023-10-10 11:18:58 +0630

    Implemented Security Filter Chain and Password Encoder

--- src/main/java/team/placeholder/internalprojectsmanagementsystem/security/SecurityConfig.java
+++ src/main/java/team/placeholder/internalprojectsmanagementsystem/security/SecurityConfig.java
@@ -1,6 +1,88 @@
 package team.placeholder.internalprojectsmanagementsystem.security;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
 
     //TODO:implement security config
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http.authorizeHttpRequests(
+                (auth) -> auth
+                        .requestMatchers(
+                                "/resources/**",
+                                "/js/**",
+                                "/css/**",
+                                "/images/**",
+                                "/fragments/**",
+                                "/layout/**",
+                                "/login"
+                        ).permitAll()
+                        .requestMatchers(
+                                "/dashboard",
+                                "/project/**",
+                                "/department/**",
+                                "/user/list/**",
+                                "/issues/**",
+                                "/report/**"
+                        ).hasAnyRole("PMO","SDQC")
+                        .requestMatchers(
+                                "/dashboard",
+                                "/project/**",
+                                "/user/list/**",
+                                "/issues/**",
+                                "/report/**"
+                        ).hasAnyRole("DEPARTMENT_HEAD")
+                        .requestMatchers(
+                                "/dashboard",
+                                "/project/**",
+                                "/user/list/**",
+                                "/issues/**",
+                                "/report/**"
+                        ).hasAnyRole("PROJECT_MANAGER")
+                        .requestMatchers(
+                                "/dashboard",
+                                "/project/**",
+                                "/user/**",
+                                "/issues/**"
+                        ).hasAnyRole("EMPLOYEE","CONTRACT","FOC")
+                        .requestMatchers("/").authenticated()
+                        .anyRequest().authenticated()
+
+        ).exceptionHandling(
+                (exceptionHandling) -> exceptionHandling
+                        .accessDeniedPage("/accessDenied")
+        )
+
+                .formLogin(
+                        (formLogin) -> formLogin.loginPage("/login")
+                                .loginProcessingUrl("/processLogin")
+                                .successHandler(
+                                        (request, response, authentication) -> {
+                                            response.sendRedirect("/");
+                                        })
+                                .permitAll()
+                )
+                .logout(
+                        (logout) -> logout.logoutUrl("/logout").permitAll()
+                );
+        return http.build();
+
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
