6227963dc020a6f909654930b8c41e603be27496
hp <thanthtetko2204@gmail.com>
2023-10-11 14:54:18 +0630

    Model Test Required....

--- src/main/java/team/placeholder/internalprojectsmanagementsystem/model/issue/Issue.java
+++ src/main/java/team/placeholder/internalprojectsmanagementsystem/model/issue/Issue.java
@@ -2,12 +2,24 @@ package team.placeholder.internalprojectsmanagementsystem.model.issue;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
 
 @Entity
-public class Issue {
+@Table(name="issue_ledgar")
+@NoArgsConstructor
+@Getter
+@Setter
+public class Issue implements Serializable {
     @Id
     private Long id;
+    private String title;
+    private String description;
 
-    //TODO: Implement Issue Entity
+
 
 }
--- src/main/java/team/placeholder/internalprojectsmanagementsystem/model/project/Architecture.java
+++ src/main/java/team/placeholder/internalprojectsmanagementsystem/model/project/Architecture.java
@@ -1,6 +1,22 @@
 package team.placeholder.internalprojectsmanagementsystem.model.project;
 
-import java.io.Serializable;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import java.io.Serializable;
+@Entity
+@Table(name="architecture")
+@NoArgsConstructor
+@Getter
+@Setter
 public class Architecture implements Serializable {
+    @Id
+    private Long id;
+    private String tech_name;
+
+    @ManyToOne
+    @JoinColumn(name = "project_id")
+    private Project project;
 }
