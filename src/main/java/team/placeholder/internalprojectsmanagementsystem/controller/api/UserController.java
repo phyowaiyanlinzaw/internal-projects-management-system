67446fde2ea9527a94218b4c6227f102d7701ffb
Phyo Wai Yan Lin Zaw <phyowaiyanlinzaw@ucsy.edu.mm>
2023-10-11 14:27:58 +0630

    Created Essential API Controller Classes

--- src/main/java/team/placeholder/internalprojectsmanagementsystem/controller/api/UserController.java
+++ src/main/java/team/placeholder/internalprojectsmanagementsystem/controller/api/UserController.java
@@ -1,13 +1,12 @@
 package team.placeholder.internalprojectsmanagementsystem.controller.api;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/user/")
 public class UserController {
 
-    @GetMapping("/")
-    public String index() {
-        return "Hello World!";
-    }
 }
