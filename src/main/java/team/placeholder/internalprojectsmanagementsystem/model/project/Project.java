b6d04c0d7a9f04076096e03388e26945440d66a1
hp <thanthtetko2204@gmail.com>
2023-10-11 14:36:57 +0630

    Model Test Required....

--- src/main/java/team/placeholder/internalprojectsmanagementsystem/model/project/Project.java
+++ src/main/java/team/placeholder/internalprojectsmanagementsystem/model/project/Project.java
@@ -1,11 +1,31 @@
 package team.placeholder.internalprojectsmanagementsystem.model.project;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import team.placeholder.internalprojectsmanagementsystem.model.user.Client;
+
+import java.io.Serializable;
+import java.sql.Date;
 
 @Entity
-public class Project {
+@Table(name="project")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Project implements Serializable {
     @Id
     private Long id;
+    private String name;
+    private String backgroung;
+    private int duration;
+    private Date start_date;
+    private Date end_date;
+    private String objective;
+
+    @ManyToOne
+    @JoinColumn(name="client_id")
+    private Client client;
 
 }
