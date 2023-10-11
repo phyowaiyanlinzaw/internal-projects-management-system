3b6ef5ad9f2fdd9341543df979b9a8c5985b2eab
Phyo Wai Yan Lin Zaw <phyowaiyanlinzaw@ucsy.edu.mm>
2023-10-11 14:39:16 +0630

    Created Essential API Controller Classes

---
+++ src/main/java/team/placeholder/internalprojectsmanagementsystem/controller/api/IssueController.java
@@ -0,0 +1,9 @@
+package team.placeholder.internalprojectsmanagementsystem.controller.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/issue/")
+public class IssueController {
+}
