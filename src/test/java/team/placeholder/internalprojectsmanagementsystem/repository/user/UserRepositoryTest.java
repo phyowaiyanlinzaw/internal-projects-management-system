f813efcac43744c9330b0df9e3b2bca604ce545b
HninAyeLwin1331 <nunhun879@gmail.com>
2023-10-11 20:22:09 +0630

    User Testing

---
+++ src/test/java/team/placeholder/internalprojectsmanagementsystem/repository/user/UserRepositoryTest.java
@@ -0,0 +1,7 @@
+package team.placeholder.internalprojectsmanagementsystem.repository.user;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserRepositoryTest {
+    //TODO: Implement User Testing
+}
