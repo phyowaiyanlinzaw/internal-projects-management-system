16ecdc060195fa5c557057ddd69389669968835c
HninAyeLwin1331 <nunhun879@gmail.com>
2023-10-11 10:45:13 +0630

    class build

---
+++ src/main/java/team/placeholder/internalprojectsmanagementsystem/model/user/Client.java
@@ -0,0 +1,10 @@
+package team.placeholder.internalprojectsmanagementsystem.model.user;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Client {
+    @Id
+    private long id;
+}
