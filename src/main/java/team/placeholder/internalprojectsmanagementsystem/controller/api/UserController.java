0dc49812a697705f1654086536a9c612e3b2d374
Phyo Wai Yan Lin Zaw <phyowaiyanlinzaw@ucsy.edu.mm>
2023-10-11 11:06:54 +0630

    Added URL for /

--- src/main/java/team/placeholder/internalprojectsmanagementsystem/controller/api/UserController.java
+++ src/main/java/team/placeholder/internalprojectsmanagementsystem/controller/api/UserController.java
@@ -1,9 +1,13 @@
 package team.placeholder.internalprojectsmanagementsystem.controller.api;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
 
-    //TODO: Implement user api controller
+    @GetMapping("/")
+    public String index() {
+        return "Hello World!";
+    }
 }
