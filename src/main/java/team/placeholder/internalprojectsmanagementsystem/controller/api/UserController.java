e5079ef71cc868cee6f6ea546672025e5cd9ae56
Ei Ei Phyo <ep5370650@gmail.com>
2023-10-12 10:35:59 +0630

    controller

--- src/main/java/team/placeholder/internalprojectsmanagementsystem/controller/api/UserController.java
+++ src/main/java/team/placeholder/internalprojectsmanagementsystem/controller/api/UserController.java
@@ -1,12 +1,31 @@
 package team.placeholder.internalprojectsmanagementsystem.controller.api;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import team.placeholder.internalprojectsmanagementsystem.dto.model.user.UserDto;
+import team.placeholder.internalprojectsmanagementsystem.service.user.UserService;
 
 @RestController
 @RequestMapping("/api/user/")
 public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity getUserById(@PathVariable Long id) {
+        List<UserDto> users = userService.getUserById(id);
+        if(users.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok(users);
+    }
+
+    @PostMapping("create")
+    public String create (@RequestBody String user)
+
 
 }
