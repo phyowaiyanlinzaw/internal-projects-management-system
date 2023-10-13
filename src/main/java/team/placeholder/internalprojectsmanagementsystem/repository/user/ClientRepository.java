bdfb7a67f00333a93cf22cd994e61c9bf9b523f9
HninAyeLwin1331 <nunhun879@gmail.com>
2023-10-13 09:42:17 +0630

    repo

--- src/main/java/team/placeholder/internalprojectsmanagementsystem/repository/user/ClientRepository.java
+++ src/main/java/team/placeholder/internalprojectsmanagementsystem/repository/user/ClientRepository.java
@@ -3,7 +3,20 @@ package team.placeholder.internalprojectsmanagementsystem.repository.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import team.placeholder.internalprojectsmanagementsystem.model.user.Client;
+import team.placeholder.internalprojectsmanagementsystem.model.user.User;
+
+import java.util.List;
 
 public interface ClientRepository extends JpaRepository<Client,Long> {
+
+
+    List<Client> selectAllUser();
+    Client findById(long id);
+
+    Client findByName(String name);
+
+    List <Client> selectAllClientById(int id);
+
+
 
 }
