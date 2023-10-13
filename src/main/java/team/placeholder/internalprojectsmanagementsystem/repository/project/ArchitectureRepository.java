aaad8186d0f1e77070c731f163b70725074d5bf6
HninAyeLwin1331 <nunhun879@gmail.com>
2023-10-13 09:59:33 +0630

    repo

---
+++ src/main/java/team/placeholder/internalprojectsmanagementsystem/repository/project/ArchitectureRepository.java
@@ -0,0 +1,9 @@
+package team.placeholder.internalprojectsmanagementsystem.repository.project;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import team.placeholder.internalprojectsmanagementsystem.model.project.Architecture;
+
+public interface ArchitectureRepository extends JpaRepository<Architecture, Long> {
+
+}
