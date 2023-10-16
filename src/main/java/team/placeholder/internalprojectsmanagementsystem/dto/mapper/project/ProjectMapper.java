29053269afe65d85239ef2e2065b301ea34f3844
HninAyeLwin1331 <nunhun879@gmail.com>
2023-10-16 09:31:50 +0630

    controller

--- src/main/java/team/placeholder/internalprojectsmanagementsystem/dto/mapper/project/ProjectMapper.java
+++ src/main/java/team/placeholder/internalprojectsmanagementsystem/dto/mapper/project/ProjectMapper.java
@@ -1,10 +1,12 @@
 package team.placeholder.internalprojectsmanagementsystem.dto.mapper.project;
 
+import org.springframework.stereotype.Component;
 import team.placeholder.internalprojectsmanagementsystem.dto.model.project.ProjectDto;
 import team.placeholder.internalprojectsmanagementsystem.model.project.Project;
 
+@Component
 public class ProjectMapper {
-    public static ProjectDto toProjectDto(Project project){
+    public ProjectDto toProjectDto(Project project){
         if(project == null){
             return null;
         }
@@ -18,5 +20,21 @@ public class ProjectMapper {
         projectDto.setCurrent_phase(project.getCurrent_phase());
         projectDto.setObjective(project.getObjective());
         return projectDto;
+    }
+
+    public Project toProject(ProjectDto projectDto) {
+        if(projectDto == null){
+            return null;
+        }
+        Project project = new Project();
+        project.setId(projectDto.getId());
+        project.setName(projectDto.getName());
+        project.setBackground(projectDto.getBackground());
+        project.setDuration(projectDto.getDuration());
+        project.setStart_date(projectDto.getStart_date());
+        project.setEnd_date(projectDto.getEnd_date());
+        project.setCurrent_phase(projectDto.getCurrent_phase());
+        project.setObjective(projectDto.getObjective());
+        return project;
     }
 }
