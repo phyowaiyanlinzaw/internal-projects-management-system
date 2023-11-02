6b979dbbca5378597498ddedc8701a407b05a0a1
Ei Ei Phyo <ep5370650@gmail.com>
2023-11-02 22:37:51 +0630

    Issue add and details

--- src/main/java/team/placeholder/internalprojectsmanagementsystem/model/issue/issueenum/Category.java
+++ src/main/java/team/placeholder/internalprojectsmanagementsystem/model/issue/issueenum/Category.java
@@ -1,5 +1,6 @@
 package team.placeholder.internalprojectsmanagementsystem.model.issue.issueenum;
 
 public enum Category {
-    ERROR,BUG,COMMUNICATION,TECHNICAL,BUDGET;
+//    ERROR,BUG,COMMUNICATION,TECHNICAL,BUDGET,DESIGN,TESTING,OTHER;
+    bug,error,communication,technical,budget,design,testing,other;
 }
