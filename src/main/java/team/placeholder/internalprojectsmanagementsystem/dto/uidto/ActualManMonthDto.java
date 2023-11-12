e693b7a4496dbe8124dd6e6ba279a0b8b873ecda
Phyo Wai Yan Lin Zaw <phyowaiyanlinzaw@ucsy.edu.mm>
2023-11-12 21:50:59 +0630

    Dashboard Manmonth APi

---
+++ src/main/java/team/placeholder/internalprojectsmanagementsystem/dto/uidto/ActualManMonthDto.java
@@ -0,0 +1,12 @@
+package team.placeholder.internalprojectsmanagementsystem.dto.uidto;
+
+import lombok.Data;
+
+import java.util.Map;
+
+@Data
+public class ActualManMonthDto {
+    private long actualManMonthHours;
+    private String monthName;
+
+}
