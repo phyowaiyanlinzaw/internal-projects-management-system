679d415c69994320260862f2be9aed0dce82e893
Phyo Wai Yan Lin Zaw <phyowaiyanlinzaw@ucsy.edu.mm>
2023-11-13 16:36:24 +0630

    Productivity API

--- src/main/java/team/placeholder/internalprojectsmanagementsystem/controller/api/DashboardController.java
+++ src/main/java/team/placeholder/internalprojectsmanagementsystem/controller/api/DashboardController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import team.placeholder.internalprojectsmanagementsystem.dto.uidto.ActualManMonthDto;
 import team.placeholder.internalprojectsmanagementsystem.dto.uidto.PlanManMonthDto;
+import team.placeholder.internalprojectsmanagementsystem.dto.uidto.ProductivityDto;
 import team.placeholder.internalprojectsmanagementsystem.service.impl.project.TaskServiceImpl;
 
 import java.util.List;
@@ -17,19 +18,26 @@ import java.util.List;
 @RequestMapping("/api/dashboard")
 public class DashboardController {
 
-    private  final TaskServiceImpl taskService;
+    private final TaskServiceImpl taskService;
 
     @GetMapping("/actual-man-month/project/{projectId}")
     public ResponseEntity<List<ActualManMonthDto>> getMonthlyManMonth(
             @PathVariable("projectId") long projectId
     ) {
         return ResponseEntity.ok(taskService.calculateMonthlyActualManHoursFromTasks(projectId));
-        }
+    }
 
-        @GetMapping("/plan-man-month/project/{projectId}")
-        public ResponseEntity<List<PlanManMonthDto>> getMonthlyPlanManMonth(
-                @PathVariable("projectId") long projectId
-        ) {
-            return ResponseEntity.ok(taskService.calculateMonthlyPlanManHoursFromTasks(projectId));
-        }
+    @GetMapping("/plan-man-month/project/{projectId}")
+    public ResponseEntity<List<PlanManMonthDto>> getMonthlyPlanManMonth(
+            @PathVariable("projectId") long projectId
+    ) {
+        return ResponseEntity.ok(taskService.calculateMonthlyPlanManHoursFromTasks(projectId));
+    }
+
+    @GetMapping("/productivity/project/{projectId}")
+    public ResponseEntity<List<ProductivityDto>> getProductivity(
+            @PathVariable("projectId") long projectId
+    ) {
+        return null;
+    }
 }
