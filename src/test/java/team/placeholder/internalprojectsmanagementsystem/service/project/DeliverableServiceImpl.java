47307050612077ef3a22d5023d252da0e8d482da
Ei Ei Phyo <ep5370650@gmail.com>
2023-11-14 20:06:46 +0630

    validation test

--- src/test/java/team/placeholder/internalprojectsmanagementsystem/service/project/DeliverableServiceImpl.java
+++ src/test/java/team/placeholder/internalprojectsmanagementsystem/service/project/DeliverableServiceImpl.java
@@ -1,12 +1,10 @@
 package team.placeholder.internalprojectsmanagementsystem.service.project;
 
 import lombok.RequiredArgsConstructor;
+
+
 import org.springframework.stereotype.Service;
-import team.placeholder.internalprojectsmanagementsystem.dto.mapper.department.DepartmentMapper;
-import team.placeholder.internalprojectsmanagementsystem.dto.mapper.project.DeliverableMapper;
 import team.placeholder.internalprojectsmanagementsystem.dto.model.project.DeliverableDto;
-import team.placeholder.internalprojectsmanagementsystem.model.department.Department;
-import team.placeholder.internalprojectsmanagementsystem.model.project.Deliverable;
 import team.placeholder.internalprojectsmanagementsystem.repository.project.DeliverableRepository;
 
 import java.util.List;
