4f35a20debea0f9dffd642bc9a5d889bd87e044a
zawlinshein <pudgeismyfavourate@gmail.com>
2023-11-18 20:40:08 +0630

    created new look for issue and project list so trying to implement them

---
+++ src/main/java/team/placeholder/internalprojectsmanagementsystem/dto/uidto/ProListDto.java
@@ -0,0 +1,34 @@
+package team.placeholder.internalprojectsmanagementsystem.dto.uidto;
+
+import java.util.List;
+
+import lombok.Getter;
+import lombok.Setter;
+import team.placeholder.internalprojectsmanagementsystem.dto.model.project.TasksDto;
+import team.placeholder.internalprojectsmanagementsystem.dto.model.user.UserDto;
+
+@Getter
+@Setter
+public class ProListDto {
+    
+    private long id;
+    private String projectName;
+    private long percentage;
+    private long startDate;
+    private long endDate;
+    private UserDto user;
+    private List<TasksDto> tasks;
+
+    public ProListDto() {}
+
+    public ProListDto(long id, String projectName, long percentage, long startDate, long endDate, UserDto user, List<TasksDto> tasks) {
+        this.id = id;
+        this.projectName = projectName;
+        this.percentage = percentage;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.user = user;
+        this.tasks = tasks;
+    }
+    
+}
