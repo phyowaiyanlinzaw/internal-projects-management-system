817c5fe19c92cf71daa7aa7482aece64fea3c876
hp <thanthtetko2204@gmail.com>
2023-11-26 00:11:02 +0630

    Testing ......

--- src/main/java/team/placeholder/internalprojectsmanagementsystem/dto/model/user/ClientDto.java
+++ src/main/java/team/placeholder/internalprojectsmanagementsystem/dto/model/user/ClientDto.java
@@ -12,5 +12,35 @@ public class ClientDto implements Serializable {
     private long id;
     private String name,email,phone;
 
-    
+    public static class ClientDtoBuilder {
+        private ClientDto clientDto;
+
+        public ClientDtoBuilder() {
+            this.clientDto = new ClientDto();
+        }
+
+        public ClientDtoBuilder id(long id) {
+            this.clientDto.setId(id);
+            return this;
+        }
+
+        public ClientDtoBuilder name(String name) {
+            this.clientDto.setName(name);
+            return this;
+        }
+
+        public ClientDtoBuilder email(String email) {
+            this.clientDto.setEmail(email);
+            return this;
+        }
+
+        public ClientDtoBuilder phone(String phone) {
+            this.clientDto.setPhone(phone);
+            return this;
+        }
+
+        public ClientDto build() {
+            return this.clientDto;
+        }
+    }
 }
--- src/main/java/team/placeholder/internalprojectsmanagementsystem/model/user/Client.java
+++ src/main/java/team/placeholder/internalprojectsmanagementsystem/model/user/Client.java
@@ -38,6 +38,36 @@ public class Client implements Serializable {
     public int hashCode() {
         return Objects.hash(id);
     }
+    public static class ClientBuilder {
+        private Client client;
 
+        public ClientBuilder() {
+            this.client = new Client();
+        }
+
+        public ClientBuilder id(long id) {
+            this.client.setId(id);
+            return this;
+        }
+
+        public ClientBuilder name(String name) {
+            this.client.setName(name);
+            return this;
+        }
+
+        public ClientBuilder phone(String phone) {
+            this.client.setPhone(phone);
+            return this;
+        }
+
+        public ClientBuilder email(String email) {
+            this.client.setEmail(email);
+            return this;
+        }
+
+        public Client build() {
+            return this.client;
+        }
+    }
 
 }
