ce80f5ba75e71996f23b53a58f2faf83875181b6
HninAyeLwin1331 <nunhun879@gmail.com>
2023-11-29 00:13:35 +0630

    noti test repair

--- src/test/java/team/placeholder/internalprojectsmanagementsystem/dto/uidto/NotiDtoTest.java
+++ src/test/java/team/placeholder/internalprojectsmanagementsystem/dto/uidto/NotiDtoTest.java
@@ -1,7 +1,35 @@
 package team.placeholder.internalprojectsmanagementsystem.dto.uidto;
 
+import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-class NotiDtoTest {
+public class NotiDtoTest {
 
+    @Test
+    public void testGetterAndSetter() {
+        // Create a NotiDto
+        NotiDto notiDto = new NotiDto();
+        notiDto.setId(1L);
+        notiDto.setDescription("Notification description");
+        notiDto.setNoti_time(1636600000000L); // Assuming a timestamp in milliseconds
+
+        // Verify that the getters return the expected values
+        assertEquals(1L, notiDto.getId());
+        assertEquals("Notification description", notiDto.getDescription());
+        assertEquals(1636600000000L, notiDto.getNoti_time());
+
+        // Modify some values using setters
+        notiDto.setId(2L);
+        notiDto.setDescription("Updated description");
+        notiDto.setNoti_time(1636700000000L); // Assuming a different timestamp in milliseconds
+
+        // Verify that the modified values are reflected
+        assertEquals(2L, notiDto.getId());
+        assertEquals("Updated description", notiDto.getDescription());
+        assertEquals(1636700000000L, notiDto.getNoti_time());
+    }
+
+    // Add more tests for edge cases or specific scenarios
+
+    // ... rest of the tests
 }
---
+++ src/test/java/team/placeholder/internalprojectsmanagementsystem/dto/uidto/PageRequestDtoTest.java
@@ -0,0 +1,7 @@
+package team.placeholder.internalprojectsmanagementsystem.dto.uidto;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PageRequestDtoTest {
+
+}
