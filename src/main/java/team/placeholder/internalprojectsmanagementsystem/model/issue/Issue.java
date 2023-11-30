7b08e47cf0d7c34e61d69d5d3b3e249f5ae57973
Ei Ei Phyo <ep5370650@gmail.com>
2023-11-30 13:20:49 +0630

    issue

--- src/main/java/team/placeholder/internalprojectsmanagementsystem/model/issue/Issue.java
+++ src/main/java/team/placeholder/internalprojectsmanagementsystem/model/issue/Issue.java
@@ -27,10 +27,19 @@ public class Issue{
 
     @Column(columnDefinition = "TEXT")
     private String description;
+
+
     private String place;
+
+    @Column(columnDefinition = "TEXT")
     private String impact;
+
+    @Column(columnDefinition = "TEXT")
     private String root_cause;
+
+    @Column(columnDefinition = "TEXT")
     private String direct_cause;
+
     @Column(columnDefinition = "TEXT")
     private String corrective_action;
     @Column(columnDefinition = "TEXT")
