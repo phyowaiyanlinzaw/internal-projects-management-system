c1ee26fa7a8208681e2754ed9d86868875bfa623
Ei Ei Phyo <ep5370650@gmail.com>
2023-12-03 22:50:28 +0630

    final testing pr

--- src/test/java/team/placeholder/internalprojectsmanagementsystem/service/impl/user/UserServiceImplTest.java
+++ src/test/java/team/placeholder/internalprojectsmanagementsystem/service/impl/user/UserServiceImplTest.java
@@ -2,6 +2,7 @@ package team.placeholder.internalprojectsmanagementsystem.service.impl.user;
 
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Before;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.mockito.*;
@@ -45,7 +46,7 @@ class UserServiceImplTest {
     private JavaMailSender mailSender;
 
     @Mock
-    private ModelMapper modelMapper ;
+    private ModelMapper modelmapper ;
 
     @Mock
     private AESImpl aes;
@@ -56,28 +57,30 @@ class UserServiceImplTest {
     @InjectMocks
     private UserServiceImpl userService;
 
-    @Before
+    @BeforeEach
     public void setUp() {
         MockitoAnnotations.openMocks(this);
-
     }
 
     @Test
-    public void testGetAllUsers() {
-        // Create test data
+    void testGetAllUsers() {
+        // Mock data
         User user = new User();
         user.setId(1L);
         user.setName("John Doe");
         user.setEmail("john.doe@example.com");
         user.setRole(Role.EMPLOYEE);
 
-        when(userRepository.findAll()).thenReturn(Collections.singletonList(user));
+        List<User> userList = Collections.singletonList(user);
+        when(userRepository.findAll()).thenReturn(userList);
 
-        DepartmentDto departmentDto = new DepartmentDto(); // You need to create a DepartmentDto object
-        when(modelMapper.map(Mockito.any(Department.class), Mockito.eq(DepartmentDto.class))).thenReturn(departmentDto);
+        DepartmentDto departmentDto = new DepartmentDto();
+        when(modelmapper.map(any(Department.class), eq(DepartmentDto.class))).thenReturn(departmentDto);
 
-        UserDto userDto = new UserDto(); // You need to create a UserDto object
-        when(modelMapper.map(Mockito.any(User.class), Mockito.eq(UserDto.class))).thenReturn(userDto);
+        UserDto userDto = new UserDto();
+        when(modelmapper.map(any(User.class), eq(UserDto.class))).thenReturn(userDto);
+
+
 
         // Call the method to be tested
         List<UserDto> result = userService.getAllUsers();
@@ -103,7 +106,7 @@ class UserServiceImplTest {
         when(userRepository.findById(1L)).thenReturn(existingUser);
 
         // Mock the modelMapper.map method
-        when(modelMapper.map(userDto.getDepartmentdto(), Department.class)).thenReturn(new Department());
+        when(modelmapper.map(userDto.getDepartmentdto(), Department.class)).thenReturn(new Department());
 
         // Call the method to be tested
         UserDto result = userService.updateProfile(userDto);
@@ -137,6 +140,44 @@ class UserServiceImplTest {
 
         // Verify that the UserRepository save method was not called
 
+    }
+
+    @Test
+    public void testSave() {
+        // Arrange
+        UserDto userDto = new UserDto();
+        User user = new User();
+
+        // Mocking the behavior of modelMapper
+        when(modelmapper.map(userDto, User.class)).thenReturn(user);
+
+        // Act
+        userService.save(userDto);
+
+        // Assert
+        // Verifying that the userRepository's save method is called with the mapped user
+        verify(userRepository, times(1)).save(user);
+
+        // Verifying that modelMapper.map is called with the provided arguments
+        verify(modelmapper, times(1)).map(userDto, User.class);
+    }
+
+    @Test
+    void testCountAllByDepartmentId() {
+        // Mocking
+        long departmentId = 1L;
+        long expectedCount = 5L; // Replace with the expected count for your test case
+        UserRepository userRepository = mock(UserRepository.class);
+        when(userRepository.countAllByDepartmentId(departmentId)).thenReturn(expectedCount);
+
+        // Creating the service
+
+        // Calling the method to be tested
+        Long result = userService.countAllByDepartmentId(departmentId);
+
+        // Verifying the result
+        assertEquals(expectedCount, result);
+        verify(userRepository, times(1)).countAllByDepartmentId(departmentId);
     }
 
 
@@ -203,87 +244,117 @@ class UserServiceImplTest {
 
     @Test
     void testResetPassword_UserFound() {
-        // Mock the UserRepository to return a user when findByEmail is called
+        // Arrange
         String userEmail = "john.doe@example.com";
         User mockUser = new User();
         when(userRepository.findByEmail(userEmail)).thenReturn(mockUser);
 
-        // Call the method to be tested
+
+        // Mock the behavior of modelMapper.map to return a UserDto
+        when(modelmapper.map(mockUser, UserDto.class)).thenReturn(new UserDto());
+
+        // Act
         UserDto result = userService.resetPassword(userEmail, "newPassword");
 
+        // Assert
+        // Verify that userRepository.findByEmail is called with the correct argument
+        verify(userRepository, times(1)).findByEmail(userEmail);
+
+        // Verify that userRepository.save is called
+//        verify(userRepository, times(1)).save(any());
+
+        // Verify that modelMapper.map is called with the correct arguments
+        verify(modelmapper, times(1)).map(mockUser, UserDto.class);
+
         // Assert that the result is not null
-        assertNull(result);
+        assertNotNull(result);
         // You may want to add more specific assertions based on your requirements
     }
 
     @Test
     void testResetPassword_UserNotFound() {
-        // Mock the UserRepository to return null when findByEmail is called
+        // Arrange
         String userEmail = "nonexistent@example.com";
+
+        // Mock the behavior of userRepository.findByEmail to return null when called with userEmail
         when(userRepository.findByEmail(userEmail)).thenReturn(null);
 
-        // Call the method to be tested
+        // Act
         UserDto result = userService.resetPassword(userEmail, "newPassword");
+
+        // Assert
+        // Verify that userRepository.findByEmail is called with the correct argument
+        verify(userRepository, times(1)).findByEmail(userEmail);
 
         // Assert that the result is null since the user is not found
         assertNull(result);
-        // You may want to add more specific assertions based on your requirements
     }
 
-    @Test
-    void testSave() {
-//        // Create a userDto for testing
-//        UserDto userDto = new UserDto();
-//        userDto.setId(1L);
-//        userDto.setName("John Doe");
-//        userDto.setEmail("john.doe@example.com");
-//
-//        // Mock the UserRepository save method
-//        when(userRepository.save(any(User.class))).thenReturn(new User());
-//
-//        // Call the method to be tested
-//        UserDto result = userService.save(userDto);
-//
-//        // Assert that the result is not null
-//        assertNotNull(result);
-//        // You may want to add more specific assertions based on your requirements
-//
-//        // Verify that the UserRepository save method was called
-//        verify(userRepository, times(1)).save(any(User.class));
-    }
-    @Test
-    void testChangePassword_Success() {
-        // Create a user for testing
-//        String userEmail = "john.doe@example.com";
-//        String oldPassword = "oldPassword";
-//        String newPassword = "newPassword";
-//
-//        User mockUser = new User();
-//        mockUser.setEmail(userEmail);
-//        mockUser.setPassword(new BCryptPasswordEncoder().encode(oldPassword));
-//
-//        // Mock the UserRepository to return the user when findByEmail is called
-//        when(userRepository.findByEmail(userEmail)).thenReturn(mockUser);
-//
-//        // Call the method to be tested
-//        UserDto result = userService.changePassword(userEmail, oldPassword, newPassword);
-//
-//        // Assert that the result is not null
-//        assertNotNull(result);
-//        // You may want to add more specific assertions based on your requirements
-//
-//        // Verify that the UserRepository save method was called
-//        verify(userRepository, times(1)).save(any(User.class));
-    }
 
+    @Test
+    public void testGetUserById() {
+        // Arrange
+        long userId = 1L;
+        User user = new User();
+        user.setId(userId);
+        user.setName("John Doe");
+        user.setEmail("john.doe@example.com");
+
+        UserDto userDto = new UserDto();
+        userDto.setId(userId);
+        userDto.setName("John Doe");
+        userDto.setEmail("john.doe@example.com");
+
+        Department department = new Department();
+        department.setId(1L);
+        department.setName("IT");
+
+        DepartmentDto departmentDto = new DepartmentDto();
+        departmentDto.setId(1L);
+        departmentDto.setName("IT");
+
+        user.setDepartment(department);
+
+        // Mocking the behavior of userRepository.findById
+        when(userRepository.findById(userId)).thenReturn(user);
+
+        // Mocking the behavior of modelMapper
+        when(modelmapper.map(user, UserDto.class)).thenReturn(userDto);
+        when(modelmapper.map(user.getDepartment(), DepartmentDto.class)).thenReturn(departmentDto);
+
+        // Act
+        UserDto result = userService.getUserById(userId);
+
+        // Assert
+        // Verifying that userRepository.findById is called with the provided userId
+        verify(userRepository, times(1)).findById(userId);
+
+        // Verifying that modelMapper.map is called with the correct arguments
+        verify(modelmapper, times(1)).map(user, UserDto.class);
+        verify(modelmapper, times(1)).map(user.getDepartment(), DepartmentDto.class);
+
+        // Assertions
+        assertEquals(userDto.getId(), result.getId());
+        assertEquals(userDto.getName(), result.getName());
+        assertEquals(userDto.getEmail(), result.getEmail());
+        assertNotNull(result.getDepartmentdto());
+        assertEquals(departmentDto.getId(), result.getDepartmentdto().getId());
+        assertEquals(departmentDto.getName(), result.getDepartmentdto().getName());
+    }
     @Test
     void testChangePassword_UserNotFound() {
-        // Mock the UserRepository to return null when findByEmail is called
+        // Arrange
         String userEmail = "nonexistent@example.com";
+
+        // Mock the behavior of userRepository.findByEmail to return null when called with userEmail
         when(userRepository.findByEmail(userEmail)).thenReturn(null);
 
-        // Call the method to be tested
+        // Act
         UserDto result = userService.changePassword(userEmail, "oldPassword", "newPassword");
+
+        // Assert
+        // Verify that userRepository.findByEmail is called with the correct argument
+        verify(userRepository, times(1)).findByEmail(userEmail);
 
         // Assert that the result is null since the user is not found
         assertNull(result);
@@ -310,60 +381,52 @@ class UserServiceImplTest {
         assertNull(result);
     }
 
-    @Test
-    void testGetUserById() {
-        Long userId = 1L;
-        User mockUser = new User();
-        mockUser.setId(1L);
-        mockUser.setName("John Doe");
-
-        // Mock the UserRepository to return the user when findByEmail is called
-        when(userRepository.findById(userId)).thenReturn(Optional.of(mockUser));
-
-        // Mock the modelMapper.map method
-        when(modelMapper.map(mockUser, UserDto.class)).thenReturn(new UserDto());
-
-        // Call the method to be tested
-        UserDto result = userService.getUserById(userId);
-
-        // Assert that the result is not null
-        assertNull(result);
-
-    }
 
     @Test
     void testGetUserByEmail_UserFound() {
-        // Create a user for testing
+        // Arrange
         String userEmail = "john.doe@example.com";
+
+        // Create a mock user
         User mockUser = new User();
         mockUser.setEmail(userEmail);
         mockUser.setName("John Doe");
 
-        // Mock the UserRepository to return the user when findByEmail is called
+        // Mock the behavior of userRepository.findByEmail to return the user when called with userEmail
         when(userRepository.findByEmail(userEmail)).thenReturn(mockUser);
 
-        // Mock the modelMapper.map method
-        when(modelMapper.map(mockUser, UserDto.class)).thenReturn(new UserDto());
+        // Mock the behavior of modelMapper.map to return a new UserDto
+        when(modelmapper.map(mockUser, UserDto.class)).thenReturn(new UserDto());
 
-        // Call the method to be tested
+        // Act
         UserDto result = userService.getUserByEmail(userEmail);
 
+        // Assert
+        // Verify that userRepository.findByEmail is called with the correct argument
+        verify(userRepository, times(1)).findByEmail(userEmail);
+
+        // Verify that modelMapper.map is called with the correct arguments
+        verify(modelmapper, times(1)).map(mockUser, UserDto.class);
+
         // Assert that the result is not null
-        assertNull(result);
+        assertNotNull(result);
         // You may want to add more specific assertions based on your requirements
-
-
-
     }
 
     @Test
     void testGetUserByEmail_UserNotFound() {
-        // Mock the UserRepository to return null when findByEmail is called
+        // Arrange
         String userEmail = "nonexistent@example.com";
+
+        // Mock the behavior of userRepository.findByEmail to return null when called with userEmail
         when(userRepository.findByEmail(userEmail)).thenReturn(null);
 
-        // Call the method to be tested
+        // Act
         UserDto result = userService.getUserByEmail(userEmail);
+
+        // Assert
+        // Verify that userRepository.findByEmail is called with the correct argument
+        verify(userRepository, times(1)).findByEmail(userEmail);
 
         // Assert that the result is null since the user is not found
         assertNull(result);
