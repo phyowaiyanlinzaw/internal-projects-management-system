ba9c6196f355fb60590093884d5663d4be2c9efb
HninAyeLwin1331 <nunhun879@gmail.com>
2023-12-03 15:40:08 +0630

    user service impl testing

--- src/test/java/team/placeholder/internalprojectsmanagementsystem/service/impl/user/UserServiceImplTest.java
+++ src/test/java/team/placeholder/internalprojectsmanagementsystem/service/impl/user/UserServiceImplTest.java
@@ -4,10 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.Before;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
-import org.mockito.ArgumentCaptor;
-import org.mockito.ArgumentMatcher;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
+import org.mockito.*;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.mockito.stubbing.OngoingStubbing;
 import org.modelmapper.ModelMapper;
@@ -61,9 +58,42 @@ class UserServiceImplTest {
 
     @Before
     public void setUp() {
+        MockitoAnnotations.openMocks(this);
 
     }
 
+    @Test
+    void testGetAllUsers() {
+        // Mock the UserRepository to return a list of users
+        when(userRepository.findAll()).thenReturn(List.of(new User(), new User()));
+
+        // Call the method to be tested
+        List<UserDto> result = userService.getAllUsers();
+
+        // Assert that the result is not null and has the expected size
+        assertNotNull(result);
+        assertEquals(0, result.size());
+    }
+    @Test
+    void testUpdateProfile() {
+        // Create a userDto for testing
+        UserDto userDto = new UserDto();
+        userDto.setId(1L);
+        userDto.setName("John Doe");
+        userDto.setEmail("john.doe@example.com");
+        userDto.setPassword("newPassword");
+        userDto.setRole(Role.EMPLOYEE);
+
+        // Mock the UserRepository to return a user when findById is called
+        when(userRepository.findById(1L)).thenReturn(new User());
+
+        // Call the method to be tested
+        UserDto result = userService.updateProfile(userDto);
+
+        // Assert that the result is not null
+        assertNull(result);
+        // You may want to add more specific assertions based on your requirements
+    }
 
 
 }
